9b9cc6dca8e7218e33c890f36a78bdaa50a0fb05
Jorge Martins <77112497+jorginskn@users.noreply.github.com>
2024-11-26 22:27:35 -0300

    feat: Implementando padrão repository e repository generic

---
+++ src/FC.Codeflix.Catalog.Domain/SeedWork/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using FC.Codeflix.Catalog.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FC.Codeflix.Catalog.Domain.SeedWork;
+public interface IGenericRepository<TAggregate>: IRepository
+{
+    public Task Insert(TAggregate aggregate, CancellationToken cancellationToken);
+
+}
