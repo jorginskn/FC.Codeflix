11b2a2ef51b7e22a96fc807b90f7e614f567f030
Jorge Martins <77112497+jorginskn@users.noreply.github.com>
2024-12-16 11:30:13 -0300

    feat: Criação de testes CreateCategory Throw

--- tests/FC.Codeflix.Catalog.UnitTests/Application/CreateCategory/CreateCategoryTest.cs
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/CreateCategory/CreateCategoryTest.cs
@@ -1,38 +1,83 @@
-﻿using FC.Codeflix.Catalog.Application.Interfaces;
+﻿using FC.Codeflix.Catalog.Application.UseCases.Category.CreateCategory;
 using FC.Codeflix.Catalog.Domain.Entity;
-using FC.Codeflix.Catalog.Domain.Repository;
+using FC.Codeflix.Catalog.Domain.Exceptions;
+using FC.Codeflix.Catalog.UnitTests.Application.CreateCategory;
 using FluentAssertions;
 using Moq;
-using Xunit;
 using UseCases = FC.Codeflix.Catalog.Application.UseCases.Category.CreateCategory;
 
 
 namespace FC.Codeflix.Catalog.UnitTests.Application;
+
+[Collection(nameof(CreateCategoryTestFixture))]
 public class CreateCategoryTest
 {
+    private readonly CreateCategoryTestFixture _fixture;
+
+    public CreateCategoryTest(CreateCategoryTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
     [Fact(DisplayName = nameof(CreateCategory))]
     [Trait("Application", "Create Category - Use Cases")]
     public async void CreateCategory()
     {
-        var repositoryMock = new Mock<ICategoryRepository>();
-        var unitOfWorkMock = new Mock<IUnitOfWork>();
-        var useCase =  new UseCases.CreateCategory(repositoryMock.Object, unitOfWorkMock.Object);
-        var input = new UseCases.CreateCategoryInput(
-            "Category Name",
-            "Category Description",
-            true);
+        var repositoryMock = _fixture.GetRepositoryMock();
+        var unitOfWorkMock = _fixture.GetUnitOfWorkMock();
+        var useCase = new UseCases.CreateCategory(repositoryMock.Object, unitOfWorkMock.Object);
+        var input = _fixture.GetInput();
         var output = await useCase.Handle(input, CancellationToken.None);
         repositoryMock.Verify(repository => repository.Insert(
-            It.IsAny<Category>(), 
-            It.IsAny<CancellationToken>()), 
+            It.IsAny<Category>(),
+            It.IsAny<CancellationToken>()),
             Times.Once);
         unitOfWorkMock.Verify(uow => uow.Commit(It.IsAny<CancellationToken>()), Times.Once);
 
         output.Should().NotBeNull();
-        output.Name.Should().Be("Category Name");
-        output.Description.Should().Be("Category Description");
-        output.IsActive.Should().Be(true);
+        output.Name.Should().Be(input.Name);
+        output.Description.Should().Be(input.Description);
+        output.IsActive.Should().Be(input.IsActive);
         output.Id.Should().NotBeEmpty();
-        output.CreatedAt.Should().NotBeSameDateAs(default(DateTime)) ;
-     }
+        output.CreatedAt.Should().NotBeSameDateAs(default(DateTime));
+    }
+
+    [Theory(DisplayName = nameof(ThrowWhenCantInstantiateAggregate))]
+    [Trait("Application", "Create Category - Use Cases")]
+    [MemberData(nameof(GetInvalidInputs))]
+    public async void ThrowWhenCantInstantiateAggregate(CreateCategoryInput input, string exceptionMessage)
+    {
+        var useCase = new UseCases.CreateCategory(_fixture.GetRepositoryMock().Object, _fixture.GetUnitOfWorkMock().Object);
+        Func<Task> task = async () =>  await useCase.Handle(input, CancellationToken.None);
+        await task.Should().ThrowAsync<EntityValidationException>().WithMessage(exceptionMessage);
+
+    }
+
+    public static IEnumerable<object[]> GetInvalidInputs()
+    {
+        var fixture = new CreateCategoryTestFixture();
+        var invalidInputsList = new List<object[]>();
+        var invalidInputShortName = fixture.GetInput();
+        invalidInputShortName.Name = invalidInputShortName.Name.Substring(0, 2);
+        invalidInputsList.Add(new object[]
+        {
+            invalidInputShortName,
+            $"Name should be at leats 3 characters long"
+        });
+
+
+
+        var invalidInputTooLongName = fixture.GetInput();
+        var tooLongNameForCategory = fixture.Faker.Commerce.ProductName();
+        while (tooLongNameForCategory.Length <= 255)
+            tooLongNameForCategory = $"{tooLongNameForCategory} {fixture.Faker.Commerce.ProductName()}";
+      invalidInputTooLongName.Name = tooLongNameForCategory;
+        invalidInputsList.Add(new object[] {
+            invalidInputTooLongName,
+            "Name should be less or equal 255 characters long"
+        });
+        return invalidInputsList;
+    }
+
+
 }
